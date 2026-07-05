c62c4d84b72968f9715a8bbd49648cbdfff2f0af
samruddhi06-coder <samruddhijogdand06@gmail.com>
2026-07-05 15:11:28 +0530

    Initial commit - Task 4 Personal Portfolio Website

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Thank you for contacting me! I will get back to you soon.");
+
+    form.reset();
+
+});
